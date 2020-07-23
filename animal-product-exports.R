bcdaacf00cc12192c688958e06fc1692c7f1709f
joewertz <jwertz@publicintegrity.org>
2020-07-23 18:38:35 +0100

    Tweaked import and viz

--- animal-product-exports.R
+++ animal-product-exports.R
@@ -3,11 +3,14 @@
 
 library("tidyverse")
 library("janitor")
-library("ggplot2")
+library("scales")
 
 # import data -------------------------------------------------------------
+Sys.setenv(TZ="America/New_York")
 
 #data from USA Trade
+all_meat_exports_92_19 <- read_csv("C:/data/meat-production/exports/all_meat_92_19.csv," skip = 4) %>% 
+  clean_names()
 egg_exports_92_19 <- read_csv("C:/data/meat-production/exports/egg_exports_92_19.csv", skip = 4) %>%
   clean_names()
 milk_exports_92_19 <- read_csv("C:/data/meat-production/exports/milk_exports_92_19.csv", skip = 4) %>% 
@@ -24,7 +27,9 @@ major_animal_product_exports <- rbind(egg_exports_92_19, milk_exports_92_19, pou
 
 # some light viz ----------------------------------------------------------
 
-egg_exports_92_19 %>%
+theme_set(theme_minimal())
+poultry_exports_92_19 %>%
   ggplot(aes(x = time,
-             y = value_us)) +
-  geom_line()
+             y = value_us,
+             color = commodity)) +
+  geom_line() + scale_y_continuous(labels = comma)
